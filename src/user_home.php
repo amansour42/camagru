217860f22763aaa1ac7fa1405ee25b2f8d2fbd92
Amina MANSOUR <amansour@e1r3p21.42.fr>
2018-04-12 11:26:49 +0200

    2

--- src/user_home.php
+++ src/user_home.php
@@ -49,15 +49,21 @@ if (isset($_GET['id']) AND $_GET['id'] > 0)
     <meta charset="utf-8" />
     <title>Page De Profil</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="../style/home.css" />
+    <link rel="stylesheet" href="../style/user.css" />
 </head>
 <body>
+<div class="first">
+        <a class="right" href="edition.php"> Editer Mon Profil </a>
+        <img class="logo" src="../img/logo42.png" alt="logo42" title="42">
+        <a class= "left" href="deconection.php">Se Deconnecter </a>
+	</div>
     <?php
         if (isset($_FILES['image']) AND !empty($_FILES['image']))
         {?>
         <img src=<?php echo $chemin;?> width="100px" height = "100px">    
         <?php } ?>
-    <a href="edition.php"> Editer Mon Profil </a>
-    <a href="deconection.php">Se Deconnecter </a>
+    
     <label>Prendre une Photo</label>
     <form method"post" action="" enctype="multipart/form-data">
         <input type="file" name="image" />
